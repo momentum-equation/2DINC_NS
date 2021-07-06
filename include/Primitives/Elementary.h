ddf67a1b2d699c6a18b61602b01840c00066b714
momentum-equation <surajkoolkarni@yahoo.com>
2021-07-06 16:04:26 +0530

    Add math constants

--- include/Primitives/Elementary.h
+++ include/Primitives/Elementary.h
@@ -1,6 +1,17 @@
 #pragma once
 #include <cstdint>
+#include <cmath>
+
 #include <blitz/tinyvec2.h>
 
 typedef int label;
 typedef uint32_t dim;
+
+namespace basic::math_constants
+{
+    constexpr double pi = M_PI;
+    constexpr double half_pi = 0.5*M_PI;
+    constexpr double two_pi = 2.0*M_PI;
+    
+    constexpr double e = M_E;
+}
